bdf500403b434b65171bae5fbf11aa007e2b61ba
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2020-12-01 23:56:55 +0600

    added comment model

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+
+	validates :title, presence: true, uniqueness: true
+
+	belongs_to :user
+	has_many :comments, dependent: :destroy
+
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+validates :name, presence: true, uniqueness: true, length: {minimum: 5}
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+	
 end
